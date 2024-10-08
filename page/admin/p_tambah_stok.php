ddb73081837e57a34c8f7f16a65d64d3768e0731
Alvita Azalia <alvita.azalia@student.president.ac.id>
2024-10-08 10:01:59 +0700

    Update p_tambah_stok.php

--- page/admin/p_tambah_stok.php
+++ page/admin/p_tambah_stok.php
@@ -1,37 +1,33 @@
 <?php
-// Masukkan file koneksi database
 include "../../db.php";
 
-// Periksa apakah data $_POST['id_buku'] dan $_POST['stok'] telah diatur
 if(isset($_POST['id_buku'], $_POST['stok'])) {
-    // Tangkap nilai $_POST['id_buku'] dan $_POST['stok']
+    // tangkap nilai $_POST['id_buku'] dan $_POST['stok']
     $id_buku = $_POST['id_buku'];
     $stok = $_POST['stok'];
     
-    // Buat koneksi ke database
     $koneksi = mysqli_connect('localhost', 'root', '', 'bookselling');
 
-    // Periksa koneksi database
     if (!$koneksi) {
         die("Koneksi database gagal: " . mysqli_connect_error());
     }
     
-    // Query untuk menambahkan data ke tabel stok
+    // Query buat nambahindata ke tabel stok
     $query = "INSERT INTO stok (id_buku, stok) VALUES ('$id_buku', '$stok')";
     
-    // Eksekusi query
+    // eksekusi query
     if(mysqli_query($koneksi, $query)) {
-        // Redirect ke halaman buku.php jika query berhasil
+        // redirect ke halaman buku.php kaloo query berhasil
         header("location: buku.php");
     } else {
-        // Tampilkan pesan error jika query gagal
+        // tampilkan pesan error kaloquery gagal
         echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
     }
     
-    // Tutup koneksi database
+    // tutupkoneksi database
     mysqli_close($koneksi);
 } else {
-    // Tampilkan pesan jika data $_POST tidak diatur dengan benar
+    // nampilin pesan kalo data $_POST ga diatur dengan benar
     echo "Data tidak lengkap.";
 }
 ?>
